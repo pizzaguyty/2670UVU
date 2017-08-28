fe54326c0fedbc06460b093965641ea462558064
Ty <tylancarton@gmail.com>
2017-08-28 14:07:18 -0600

    8/28/2017 Movement

---
+++ Scripting2670/Assets/MoveInputScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveInputScript : MonoBehaviour {
+
+	CharacterController cc;
+
+	// Use this for initialization
+	void Start () {
+		cc = GetComponent<CharacterController>();
+		MovementScript.KeyAction = Move;
+	}
+
+	void Move (float _movement){
+		print (_movement);
+	}		
+}
---
+++ Scripting2670/Assets/MovementScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MovementScript : MonoBehaviour {
+
+	public static Action<float> KeyAction;
+
+	// Update is called once per frame
+	void Update () {
+
+		if (KeyAction != null) {
+			KeyAction (Input.GetAxis("Horizontal"));
+		}
+	}
+}
